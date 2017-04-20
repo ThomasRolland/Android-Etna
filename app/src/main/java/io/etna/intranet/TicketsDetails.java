f5c370ec7a2e09ea6010f23a2047b3c09c1ed6b1
ThomasRolland <rollan_t@etna-alternance.net>
2017-04-20 15:36:38 +0200

    Debut async TicketsDetails

--- app/src/main/java/io/etna/intranet/TicketsDetails.java
+++ app/src/main/java/io/etna/intranet/TicketsDetails.java
@@ -1,5 +1,6 @@
 package io.etna.intranet;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -22,6 +23,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import io.etna.intranet.Curl.NetworkService;
+import io.etna.intranet.Models.ActiviteModel;
 import io.etna.intranet.Models.CustomAdapterTicketDetails;
 import io.etna.intranet.Models.TicketDetailsModel;
 
@@ -61,51 +63,68 @@ public class TicketsDetails extends Fragment {
     }
 
 
-    private List<TicketDetailsModel> genererTickets(final String idTicket){
-        List<TicketDetailsModel> Tickets = new ArrayList<TicketDetailsModel>();
-        String json_string = null;
-        //requette
-        final JSONObject[] data = new JSONObject[1];
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Callable<String> callable = new Callable<String>() {
-            @Override
-            public String call() {
+
+
+    class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            List<TicketDetailsModel> Tickets = new ArrayList<TicketDetailsModel>();
+            String json_string = null;
+            //requette
+            final JSONObject[] data = new JSONObject[1];
+                        data[0] = searchCall(idTicket);
+
+                json_string = parse(data[0]);
+            JSONArray get_data = null;
+            try {
+                get_data = new JSONArray(json_string);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            for(int i = 0; i < get_data.length(); i++) {
+                JSONObject My_data = null;
                 try {
-                    data[0] = searchCall(idTicket);
+                    My_data = get_data.getJSONObject(i);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                return String.valueOf(parse(data[0]));
+                try {
+                    //ici
+                    Tickets.add(new TicketDetailsModel(My_data.getString("message"), "Le : "+My_data.getString("created_at"), My_data.getString("author_login"), My_data.getString("author_mail")));
+                    list.add(model);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        };
-        Future<String> future = executor.submit(callable);
-        try {
-            json_string = future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        executor.shutdown();
-        JSONArray get_data = null;
-        try {
-            get_data = new JSONArray(json_string);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        for(int i = 0; i < get_data.length(); i++) {
-            JSONObject My_data = null;
-            try {
-                My_data = get_data.getJSONObject(i);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            try {
-                Tickets.add(new TicketDetailsModel(My_data.getString("message"), "Le : "+My_data.getString("created_at"), My_data.getString("author_login"), My_data.getString("author_mail")));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            return null;
         }
 
-        return Tickets;
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if(list.size() > 0) {
+                adapter.notifyDataSetChanged();
+            } else {
+                Log.d("fail", "fail");
+            }
+        }
     }
 
 
